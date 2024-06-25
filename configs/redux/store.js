aa7b2c102547e1ae0efd438bfdb3ba3bccca40d4
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2024-06-25 08:45:12 +0700

    fix error

--- configs/redux/store.js
+++ configs/redux/store.js
@@ -1,13 +1,16 @@
-import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import {
+	applyMiddleware,
+	legacy_createStore as createStore,
+	compose,
+} from 'redux';
 import { rootReducer } from './reducers';
 import { thunk } from 'redux-thunk';
 import logger from 'redux-logger';
 import { createWrapper } from 'next-redux-wrapper';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 export const store = createStore(
 	rootReducer,
-	composeWithDevTools(applyMiddleware(thunk, logger))
+	compose(applyMiddleware(thunk, logger))
 );
 const makeStore = () => store;
 export const wrapper = createWrapper(makeStore);
